e7f9773029cde535f0e0d731ba1ba704c3e40bee
MVVSAIKUMAR <mvvsaikumar9@gmail.com>
2022-04-03 10:14:02 +0530

    first commit

---
+++ closures.js
@@ -0,0 +1,60 @@
+    function counter(){
+    var counter = 0;
+
+    function IncreaseCounter() {
+        return counter += 1;
+    };
+
+    return IncreaseCounter;
+}
+
+var counter = Counter();
+alert(counter());
+alert(counter());
+alert(counter());
+alert(counter());
+
+
+let count = 0;
+(function immediate() {
+  if (count === 0) {
+    let count = 1;
+    console.log(count); 
+  }
+  console.log(count); 
+})();
+for (var i = 0; i < 3; i++) {
+  setTimeout(function log() {
+    console.log(i); 
+  }, 1000);
+}
+
+var a = 12;
+(function () {
+  alert(a);
+})();
+var a = 10;
+var x = (function () {
+  var a = 12;
+  return function () {
+    alert(a);
+  };
+})();
+x();
+var globalVar = ""xyz"";
+
+(function outerFunc(outerArg) {
+    var outerVar = 'a';
+    
+    (function innerFunc(innerArg) {
+    var innerVar = 'b';
+    
+    console.log(
+        ""outerArg = "" + outerArg + ""\n"" +
+        ""innerArg = "" + innerArg + ""\n"" +
+        ""outerVar = "" + outerVar + ""\n"" +
+        ""innerVar = "" + innerVar + ""\n"" +
+        ""globalVar = "" + globalVar);
+    
+    })(456);
+})(123);
